7ca4e8d9ac95d2b6966b681ee0366ddf5ef998a3
Vinisilvab <110431983+Vinisilvab@users.noreply.github.com>
2023-07-03 10:45:17 -0300

    Add files via upload

--- TCC-Vinicius/script.js
+++ TCC-Vinicius/script.js
@@ -1,25 +1,4 @@
-let modal = document.querySelector(".cardModal")
-let btn = document.getElementById("AbrirModal")
-let fecharModal = document.querySelector(".close")
 
-btn.addEventListener("click",Abrir)
 
-function Abrir()
-{
-    modal.style.display ="block"
-}
 
-fecharModal.addEventListener("click", Fechar);
-function Fechar()
-{
-    modal.style.display ="none"
-}
-// Função para fechar o modal clicando fora dele
-function fecharCliqueFora(event) {
-    if (event.target === modal) {
-      modal.style.display = "none";
-    }
-  }
-  
-  // Adiciona o evento de clique fora do modal
-  window.addEventListener("click", fecharCliqueFora);
+
